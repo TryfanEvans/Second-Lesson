d0b7667eebe890a7110b460d711ec981483de1d2
TryfanEvans <TryfanLE@gmail.com>
2022-03-11 20:11:47 +1030

    Strange if statement

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -4,9 +4,20 @@ int identity(int array[10][10])
     {
         for (int j = 0; j < 10; j++)
         {
-            if (array[i][i] != 1 && i == j || array[i][j] != 0)
+            if (i == j)
             {
-                return 0;
+
+                if (array[i][i] != 1)
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                if (array[i][j] != 0)
+                {
+                    return 0;
+                }
             }
         }
     }
