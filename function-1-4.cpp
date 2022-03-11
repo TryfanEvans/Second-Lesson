5cf1208d0ce259780e47d4e5ae7b82cae2f72065
TryfanEvans <TryfanLE@gmail.com>
2022-03-11 20:08:01 +1030

    second fixes

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -4,6 +4,6 @@ void print_scaled_matrix(int array[3][3], int scale)
 {
     for (int i = 0; i < 3; i++)
     {
-        std::cout << array[i][0] * scale << " " << array[i][1] << " " << array[i][2] << std::endl;
+        std::cout << array[i][0] * scale << " " << array[i][1] * scale << " " << array[i][2] * scale << std::endl;
     }
 }
