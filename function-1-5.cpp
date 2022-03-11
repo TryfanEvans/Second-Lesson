31b72671c4d5302670ae36c08d08f9a07c73f907
TryfanEvans <TryfanLE@gmail.com>
2022-03-11 20:07:23 +1030

    huh

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -4,6 +4,6 @@ void print_summed_matrices(int array1[3][3], int array2[3][3])
 {
     for (int i = 0; i < 3; i++)
     {
-        std::cout << array1[i][0] + array2[i][0] << " " << array1[i][1] + array2[i][1] << " " << array1[i][2] + array2[i][2] << " " << std::endl;
+        std::cout << array1[i][0] + array2[i][0] << " " << array1[i][1] + array2[i][1] << " " << array1[i][2] + array2[i][2] << std::endl;
     }
 }
