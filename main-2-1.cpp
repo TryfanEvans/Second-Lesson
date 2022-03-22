af8b22ef2ddf8e74ca595dc19ee094659a7c7fb0
TryfanEvans <TryfanLE@gmail.com>
2022-03-22 21:25:01 +1030

    fixes

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -4,13 +4,6 @@ extern void print_sevens(int *nums, int length);
 
 int main()
 {
-    double first[3] = {2, 43, 4};
 
-    double second[3];
-    cpyda(first, second, 3);
-    for (int i = 0; i < 3; i++)
-    {
-        //     std::cout << second[i] << std::endl;
-    }
     return 0;
 }
--- main-2-2.cpp
+++ main-2-2.cpp
@@ -4,13 +4,6 @@ extern int maximum_sum(int *nums, int length);
 
 int main()
 {
-    double first[3] = {2, 43, 4};
 
-    double second[3];
-    cpyda(first, second, 3);
-    for (int i = 0; i < 3; i++)
-    {
-        //     std::cout << second[i] << std::endl;
-    }
     return 0;
 }
